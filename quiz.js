d1a32e4cf1791c3c967b671337873032c59a75bc
arnovdijk <arnovdijk@live.nl>
2023-06-07 15:50:33 +0200

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,11 @@
+document.getElementById("red-button").addEventListener("click", function() {
+    document.body.style.backgroundColor = "red";
+});
+
+document.getElementById("blue-button").addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue";
+});
+
+document.getElementById("yellow-button").addEventListener("click", function() {
+    document.body.style.backgroundColor = "yellow";
+});
